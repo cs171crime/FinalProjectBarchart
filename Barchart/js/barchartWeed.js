b3def51c38a25e58bd4b09329dcfc62e25c2b1f8
fyounus2020 <fyounus@college.harvard.edu>
2018-11-24 01:22:17 -0500

    Added Labels, still need to fix overlaid bar labels and add dyanmic filtering

--- Barchart/js/barchartWeed.js
+++ Barchart/js/barchartWeed.js
@@ -1,5 +1,6 @@
 //Sources:
 // For Bar-Chart Structure: https://blog.risingstack.com/d3-js-tutorial-bar-charts-with-javascript/
+// Watched Following Video for Help with Implementing Barchart structure: https://www.youtube.com/watch?v=Fjmxh-gnBM0
 
 
 //Set Chart Area
@@ -10,7 +11,7 @@ var margin = 60,
 //Set SVG
 var svg = d3.select("#chart-area").append("svg")
     .attr("width", width + 300)
-    .attr("height", 2000)
+    .attr("height", 2500)
     .append("g")
     .attr("transform", "translate(60,60)");
 
@@ -41,6 +42,7 @@ d3.csv("data/weedData.csv", function(error, data) {
     //Create xAxis
     var xAxis = d3.axisBottom(xScale);
 
+    //Translate Bar downwards and fix the tick labels
     svg.append("g")
         .attr("transform","translate(0," + height + ")")
         .call(xAxis)
@@ -48,15 +50,17 @@ d3.csv("data/weedData.csv", function(error, data) {
         .attr("transform", "rotate(-70)")
         .attr("dx", "-.8em")
         .attr("dy",".25em")
-        .style("text-anchor", "end");
+        .style("text-anchor", "end")
+        .style("font-size", "8px");
 
     //Create yAxis
     var yAxis = d3.axisLeft(yScale);
-    svg.append("g").call(yAxis);
+    svg.append("g")
+        .call(yAxis)
+        .style("font-size","8px");
 
-    //Draw in Bars - From Website Sourced at Header of File
+    //Draw in Bars - From First Website Sourced at Header of File
     svg.selectAll()
-    //Below Code to Sort not working?
         .data(data)
         .enter()
         .append('rect')
@@ -64,5 +68,47 @@ d3.csv("data/weedData.csv", function(error, data) {
         .attr('y', (d) => yScale(d.MarijuanaCrimes))
         .attr('height', (d) => height - yScale(d.MarijuanaCrimes))
         .attr('width', xScale.bandwidth());
+
+    //Add Labels for y axis
+    svg.append('text')
+        .attr('x', -220)
+        .attr('y', -45)
+        .attr('transform', 'rotate(-90)')
+        .attr('text-anchor', 'middle')
+        .text('Marijuana Crimes');
+
+    //Add Labels for x axis
+    svg.append('text')
+        .attr('x', width / 2 + margin)
+        .attr('y', 700)
+        .attr('text-anchor', 'middle')
+        .text('Neighborhood')
+        .style("font-size", "20px");
+
+    //Add Title for Barchart
+    svg.append('text')
+        .attr('x', width / 2 + margin)
+        .attr('y', 40)
+        .attr('text-anchor', 'middle')
+        .text('Marijuana Crimes in Boston');
+
+    //Label the bars with the data for each neighborhood - TEMP: Add Dynamic Tooltips
+    svg.selectAll()
+        .data(data)
+        .enter()
+        .append("text")
+        .text(function(d) {
+            return d.MarijuanaCrimes;
+        })
+        .attr("x", function(d) {
+            return xScale(d.neighborhood) + xScale.bandwidth()/2;
+        })
+        .attr("y", function(d) {
+            return yScale(d.MarijuanaCrimes) + 7;
+        })
+        .style("fill", "white")
+        .style("text-anchor", "middle")
+        .style("font-size", "8px");
+
 
 })
